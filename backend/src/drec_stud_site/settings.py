1e81e802c9c0e7c0ef2078d73e1bf9d5c4693fbf
alekseik1 <1alekseik1@gmail.com>
2021-09-04 23:42:08 +0300

    feat(backend): better exceptions

--- backend/src/drec_stud_site/settings.py
+++ backend/src/drec_stud_site/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    "better_exceptions.integrations.django.BetterExceptionsMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
