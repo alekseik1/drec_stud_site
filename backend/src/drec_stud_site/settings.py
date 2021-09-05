3204b86c7cac0438d025aa6f1426ce6b5438ec68
alekseik1 <1alekseik1@gmail.com>
2021-09-05 22:30:03 +0300

    revert: "feat(backend): better exceptions"
    This reverts commit 1e81e802

--- backend/src/drec_stud_site/settings.py
+++ backend/src/drec_stud_site/settings.py
@@ -71,7 +71,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    "better_exceptions.integrations.django.BetterExceptionsMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
