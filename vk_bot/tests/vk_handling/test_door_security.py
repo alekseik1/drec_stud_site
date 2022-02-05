f8b8c33019369326b37944a93881b6a61bb97b69
alekseik1 <1alekseik1@gmail.com>
2022-02-05 13:22:08 +0300

    test(security): лучше комменты, мокнуть ФПМИ апи

--- vk_bot/tests/vk_handling/test_door_security.py
+++ vk_bot/tests/vk_handling/test_door_security.py
@@ -1,3 +1,6 @@
+import asyncio
+import os
+
 import pytest
 from pytest_cases import parametrize, parametrize_with_cases
 from pytest_mock import MockFixture
@@ -24,11 +27,13 @@ async def test_admin_can_open_anytime(
     # GIVEN: server what always give `no_orders` message
     mocked_server,
 ):
-    # WHEN: admin wants to open the door
-    lockbox_mock = mocker.AsyncMock()
+    lockbox_mock = mocker.AsyncMock(return_value=(200, "test text"))
     mocker.patch("src.utils.send_signal_door_open", lockbox_mock)
+    # GIVEN FPMI server that always gives False
+    mocker.patch("src.utils.fpmi_check", mocker.AsyncMock(return_value=False))
     message_mock = mocker.AsyncMock()
     message_mock.from_id = user_id
+    # WHEN: admin wants to open the door
     await process_door_command(message_mock, service_id, "hello there", do_open=True)
     # THEN: the door opens
     lockbox_mock.assert_called_once()
@@ -48,11 +53,13 @@ async def test_usual_user_can_open_during_order(
     # GIVEN: server what always give `yes` message
     mocked_server,
 ):
-    # WHEN: regular user wants to open the door
-    lockbox_mock = mocker.AsyncMock()
+    lockbox_mock = mocker.AsyncMock(return_value=(200, "test text"))
     mocker.patch("src.utils.send_signal_door_open", lockbox_mock)
+    # GIVEN FPMI server that always gives False
+    mocker.patch("src.utils.fpmi_check", mocker.AsyncMock(return_value=False))
     message_mock = mocker.AsyncMock()
     message_mock.from_id = user_id
+    # WHEN: regular user wants to open the door
     await process_door_command(message_mock, service_id, "hello there", do_open=True)
     # THEN: the door opens
     lockbox_mock.assert_called_once()
@@ -72,11 +79,13 @@ async def test_usual_user_cannot_open_without_order(
     # GIVEN: server what always give `no_orders` message
     mocked_server,
 ):
-    # WHEN: regular user wants to open the door
-    lockbox_mock = mocker.AsyncMock()
+    lockbox_mock = mocker.AsyncMock(return_value=(200, "test text"))
     mocker.patch("src.utils.send_signal_door_open", lockbox_mock)
+    # GIVEN FPMI server that always gives False
+    mocker.patch("src.utils.fpmi_check", mocker.AsyncMock(return_value=False))
     message_mock = mocker.AsyncMock()
     message_mock.from_id = user_id
+    # WHEN: regular user wants to open the door
     await process_door_command(message_mock, service_id, "hello there", do_open=True)
     # THEN: the door does not opens
     lockbox_mock.assert_not_called()
